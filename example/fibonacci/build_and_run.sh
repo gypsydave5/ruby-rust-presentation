d183d44fb9a6f56cc0600363f19ab9a4c3d8571f
David Wickes <dave.wickes@gmail.com>
2017-05-03 20:50:03 +0100

    dash e becaus I can

--- example/fibonacci/build_and_run.sh
+++ example/fibonacci/build_and_run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 cargo build --manifest-path ./fibonacci_rust/Cargo.toml --release
 make -C ./fibonacci_c
 go build -buildmode=c-shared -o ./fibonacci_go/fibonacci.dylib ./fibonacci_go/fibonacci.go
