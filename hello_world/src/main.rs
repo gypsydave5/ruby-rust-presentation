98f22c03a7e34a9600a1c7519f53d99c1d42c5f3
David Wickes <dave.wickes@gmail.com>
2017-05-08 10:02:50 +0100

    even more work

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,7 @@
-use std::io::{ Write, Read };
-
 fn main() {
     let a_vector = gimmee_a_vector();
-    write_vector(a_vector, &mut std::io::stdout()).unwrap();
+    let a_vector = print_vector(a_vector);
+    print_vector(a_vector);
 }
 
 fn gimmee_a_vector() -> Vec<i32> {
@@ -12,30 +11,14 @@ fn gimmee_a_vector() -> Vec<i32> {
     my_vector
 }
 
-fn write_vector<W: Write>(v: Vec<i32>, w: &mut W) -> std::io::Result<()> {
-    let body = v.iter()
-        .map(|n| n.to_string())
-        .collect::<Vec<String>>()
-        .join(", ");
-
-    write!(w, "[{}]", body)
+fn print_vector(vector : Vec<i32>) -> Vec<i32> {
+    for n in vector.clone() {
+        println!("{}", n);
+    }
+    vector
 }
 
 #[test]
 fn test_gimmee_a_vector() {
     assert_eq!(gimmee_a_vector(), vec![1, 2])
-}
-
-#[test]
-fn test_write_vec() {
-    let mut read_writer = std::io::Cursor::new(Vec::new());
-    let v = vec![5, 6];
-    write_vector(v, &mut read_writer).unwrap();
-
-    read_writer.set_position(0);
-
-    let mut output = String::new();
-    read_writer.read_to_string(&mut output).unwrap();
-
-    assert_eq!(output, "[5, 6]".to_string());
 }
